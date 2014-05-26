eed15e193e9aa8ea9850db35087f76ef78f85b85
Marcus Ottosson <konstruktion@gmail.com>
2014-05-26 16:28:40 +0100

    REQ3 implemented

--- python/messaging/chat/simple/client.py
+++ python/messaging/chat/simple/client.py
@@ -12,7 +12,7 @@ context = zmq.Context()
 
 if __name__ == '__main__':
     import sys
-    who, peers = sys.argv[1], sys.argv[2:]
+    author, peers = sys.argv[1], sys.argv[2:]
 
     push = context.socket(zmq.PUSH)
     push.connect("tcp://localhost:5555")
@@ -28,22 +28,38 @@ if __name__ == '__main__':
 
     def listen():
         while True:
-            print "\r%s" % sub.recv()
-            sys.stdout.write("%s> " % who)
+            peer, data = sub.recv_multipart()
+            sys.stdout.write("\r{0}: {1}".format(peer, data))
+            sys.stdout.write(" " * 10)  # Clear line
+            sys.stdout.write("\n%s> " % author)
 
     thread = threading.Thread(target=listen)
     thread.daemon = True
     thread.start()
 
     while True:
-        sys.stdout.write("%s> " % who)
+        sys.stdout.write("%s> " % author)
         says = raw_input()
 
-        message = {
-            'who': who,
-            'time': time.asctime(),
-            'action': 'says',
-            'data': says
-        }
+        message = {'author': author,
+                   'time': time.time()}
 
-        push.send_json(message)
+        parts = says.split()
+
+        if parts[0] == 'listen':
+            sys.stdout.write("\rNow also chatting with:\n")
+            for peer in parts[1:]:
+                sub.setsockopt(zmq.SUBSCRIBE, peer)
+                sys.stdout.write("\t%s\n" % peer)
+
+        elif parts[0] == 'mute':
+            sys.stdout.write("\rMuting:\n")
+            for peer in parts[1:]:
+                sub.setsockopt(zmq.UNSUBSCRIIBE, peer)
+                sys.stdout.write("\t%s\n" % peer)
+
+        else:
+            message['action'] = 'says'
+            message['data'] = says
+
+            push.send_json(message)
--- python/messaging/chat/simple/server.py
+++ python/messaging/chat/simple/server.py
@@ -3,22 +3,46 @@ import zmq
 context = zmq.Context()
 
 
-if __name__ == '__main__':
-    pull = context.socket(zmq.PULL)
-    pull.bind("tcp://*:5555")
+class Letter(object):
+    def __init__(self, data, author, timestamp):
+        self.data = data
+        self.author = author
+        self.timestamp = timestamp
+        self.delivered = False
 
-    pub = context.socket(zmq.PUB)
-    pub.bind("tcp://*:5556")
+    @classmethod
+    def from_message(cls, message):
+        letter = cls(data=message['data'],
+                     author=message['author'],
+                     timestamp=message['time'])
+        return letter
+
+# Keep track of messages sent
+letters = {}
+
+
+if __name__ == '__main__':
+    consumer = context.socket(zmq.PULL)
+    consumer.bind("tcp://*:5555")
+
+    producer = context.socket(zmq.PUB)
+    producer.bind("tcp://*:5556")
 
     try:
         while True:
-            message = pull.recv_json()
-            if message['action'] == 'says':
+            message = consumer.recv_json()
+            letter = Letter.from_message(message)
 
-                says = "{data}".format(data=message['data'])
+            action = message['action']
+            author = message['author']
+            time = message['time']
+            data = message['data']
 
-                print "Sending %s" % says
-                pub.send_string("%s: %s" % (message['who'], says))
+            if action == 'says':
+                producer.send_multipart([str(author), str(data)])
+
+            if action == 'listen':
+                print "%s is also listening on %s" % (author, data)
 
     except KeyboardInterrupt:
         print "\nGood bye"
